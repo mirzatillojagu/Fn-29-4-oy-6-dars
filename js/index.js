1911661ca56ea6d6d9dab9fcbbbbfad106a0e0e8
mirzatillojagu <imirzatillo0@gmail.com>
2024-10-03 13:46:00 +0500

    vazifa

---
+++ js/index.js
@@ -0,0 +1,143 @@
+// 1
+// for(let i = 1; i <=5; i++){
+//     console.log(i);
+    
+// }
+
+// 2
+// for(let i = 1; i <=20; i++){
+//     console.log(i);
+    
+// }
+
+// 3
+// let num = +prompt('Sonni kiriting')
+// for(let i = 1; i<=num;i++){
+//     console.log(i+(i*i));
+// }
+
+
+// 4
+// for(let i =0; i <=10; i+=2){
+//    console.log(i);
+   
+// }
+
+// 5
+// for(let i =1; i <=15; i+=2){
+//    console.log(i);
+   
+// }
+
+
+
+// 7
+// for(let i = 15; i >=5; i--){
+//     console.log(i);
+    
+// }
+
+
+
+// 10
+// for(let i=1; i <= 50; i++){
+//      if (i % 5 == 0) {
+//         console.log(i);
+        
+//      }
+// }
+
+
+// 11
+// for(let i =100; i >= 1; i--){
+//     console.log(i);
+    
+// }
+
+
+// 12
+// let sum = 0;
+// for(let i = 1; i <= 100; i++){
+//     sum = sum + i
+// }
+
+// console.log(sum);
+
+
+// 13
+// let num1 = +prompt("1-sonni kriting")
+// let num2 = +prompt("2-sonni kriting")
+
+// for(let i =num1; i <= num2; i++){
+//     console.log(i);
+// }
+
+// 14
+// let num = +prompt("sonni kriting")
+
+// for(let i=1; i <= num; i++){
+//     if ( num % i ==0) {
+//          console.log(i);
+         
+//     }
+// }
+
+
+// 15
+// for(let i=1; i <= 20; i++){
+//     console.log(i*i);
+    
+// }
+
+
+// 16
+// for(let i=1; i <= 100; i++){
+//     if (i % 7 ==0) {
+//         console.log(i);
+        
+//     }
+// }
+
+
+// 19
+// let sum = 0;
+// for(let i=1; i <=50; i++){
+//     if (i % 2 == 1) {
+//         sum = sum + i
+//     }
+//     console.log(sum);
+// }
+    
+    
+// 20
+
+// let num1 = +prompt("1-sonni kriting")
+// let num2 = +prompt("2-sonni kriting")
+
+// for(let i=num1; i <= num2; i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+        
+//     }
+// }
+
+// 21
+// let sum = 0;
+// for(let i=1; i <= 100; i+=2){
+//     sum = sum + i
+// }
+// console.log(sum);
+
+
+
+
+// 24
+// for(let i=1; i <= 30; i++){
+//     if (i % 4 ==0 && i % 6 == 0) {
+//         console.log(i);
+        
+//     }
+// }
+
+
+
